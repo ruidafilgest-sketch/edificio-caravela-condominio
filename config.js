66723d3dd02f44ece1e37faa82ed0882b4380d9b
ruidafilgest-sketch <rui.dafilgest@gmail.com>
2026-09-09 09:42:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 var FIREBASE_CFG = {
   apiKey: "AIzaSyBBHAI-u6nuAcn2KWgoZwsavyXZY6o4JhA",
+  authDomain: "esnl-contabilidade.firebaseapp.com",
   databaseURL: "https://esnl-contabilidade-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "esnl-contabilidade",
+  storageBucket: "esnl-contabilidade.appspot.com",
+  messagingSenderId: "829122453343",
   appId: "1:829122453343:web:7a516be90941a4761320ea"
 };
